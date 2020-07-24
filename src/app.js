9e268ac84965ee4e11778d291d21848a30c1cb79
rowaidsayyed <rowaidsayyed@gmail.com>
2020-07-25 01:31:07 +0300

    finish oAuth

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import cookie from 'react-cookies';
 
 import Header from './components/header/header.js';
 import Footer from './components/footer/footer.js';
@@ -9,12 +12,18 @@ import Routes from './components/routes';
 // import Login from './auth/login/login.js';
 // import Signup from './auth/signup/signup.js';
 // import Initial from './components/initial.js';
+import * as actions from './store/signINUPReducer.js'
 
 import './reset.css';
 import './app.scss';
 
 
-function App() {
+function App(props) {
+  useEffect(()=>{
+    const cookieToken = cookie.load('auth');
+        const token = cookieToken || null;
+        props.validateToken(token);
+  },[])
   return (
     <>
       {/* <Auth capability="read"> */}
@@ -26,4 +35,16 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  sign: state.sign
+});
+const mapDispatchToProps = (dispatch, getState) => ({
+  // handleChange: (e) => dispatch(actions.handleChange(e)),
+  // login: (username, password) => dispatch(actions.login(username, password)),
+  // logout: () => dispatch(actions.logoutFun()),
+  validateToken: token => dispatch(actions.validateToken(token))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// export default App;
---
+++ src/components/oauth.js
@@ -0,0 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+
+import { connect } from 'react-redux';
+
+import * as actions from '../store/signINUPReducer'
+
+const Oauth = props => {
+  useEffect(() => {
+    props.getOauth()
+  }, [])
+
+  return (
+    <>
+
+      <div>
+
+      </div>
+
+    </>
+  )
+};
+
+const mapStateToProps = state => ({
+  sign: state.sign
+});
+
+const mapDispatchToProps = (dispatch, getState) => ({
+  getOauth:() => dispatch(actions.getOauth()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Oauth);
