471b40ddbee4b81dae5102feb37fc2d4fcf3518b
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-07-30 00:29:42 +0300

    fix form

--- src/components/signinForm/form.js
+++ src/components/signinForm/form.js
@@ -12,7 +12,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 require('dotenv').config();
 
-// import Show from '../auth/show';
 
 function SigninForm(props) {
 
@@ -132,9 +131,9 @@ function SigninForm(props) {
                                 <input type="text" className="input input--name" placeholder="Major" name="major" onChange={(e) => props.handleChange(e)} required />
                                 <input className='ChooseImage' type="file" onChange={handleChangePic} />
                                 <div className='checkBoxDiv'>
-                                    <input type="checkbox" name="terms" value="terms" id="bubble" required />
                                     <label className="bubble" for='bubble'>
-                                        I agree to the <span><a href='#'>Terms of Service</a></span> and <span><a href='#'>Privacy Policy</a></span>
+                                    <input type="checkbox" name="terms" value="terms" id="bubble" required />
+                                    Accept <span><a className="terms" href='#'>Terms of Service</a></span> and <span><a className="terms" href='#'>Privacy Policy</a></span>
                                     </label>
                                 </div>
                                 <div className="oAut">
@@ -160,7 +159,7 @@ function SigninForm(props) {
                                     <a href={props.sign.authURL} className="oauthIcon" onClick={facebookOuthFun}><span><FontAwesomeIcon icon={faFacebookF} size='2x' color='blue' /></span></a>
                                     <a href={props.sign.authURL} className="oauthIcon" onClick={googleOuthFun}> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px" height="30px"><path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" /><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" /><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" /><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z" /></svg></a>
                                 </div>
-                                <a href="/new-password">Forgot password?</a>
+                                <a className="terms" href="/new-password">Forgot password?</a>
                             </form>
                         </div>
                     </div>
