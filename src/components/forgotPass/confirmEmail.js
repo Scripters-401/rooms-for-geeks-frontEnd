426bf1865cb5221acd9b94ea08dc513e85b36723
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-08-02 02:21:59 +0300

    footer and logo

--- src/components/forgotPass/confirmEmail.js
+++ src/components/forgotPass/confirmEmail.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../store/forgotPassReducer'
-import './confirmEmail.scss';
-
+// import './confirmEmail.scss';
+import '../rooms/room.scss';
 const ConfirmEmail = props => {
 
     const handleSubmitFun = e => {
@@ -22,7 +22,45 @@ const ConfirmEmail = props => {
 
     return (
         <>
-        <h3>Password change request</h3>
+         <div className="allInall">
+                <div className="wrapper ">
+                    <div className="container">
+                        <h1 className="nameOfForm">Change Password</h1>
+                        <p className={`msg-${cssMsg}`}>{props.newPass.msgEmail}</p>
+                        <form className="form" onSubmit={(e) => handleSubmitFun(e)}>
+                            <input className="input"
+                                type="email"
+                                name="emailToFound"
+                                onChange={(e) => props.handleForgotPass(e)}
+                                placeholder="Your Email"
+                            />
+                           
+                            <input className="input"
+                                type="confirmEmail"
+                                name="confirmEmail"
+                                onChange={(e) => props.handleForgotPass(e)}
+                                placeholder="Confirm Email"
+                            />                       
+                            <button className="button" type="submit" id="login-button">SEND!</button>
+                        </form>
+                    </div>
+
+                    <ul className="bg-bubbles">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+
+        {/* <h3>Password change request</h3>
         <p className={`msg-${cssMsg}`}>{props.newPass.msgEmail}</p>
             <div>
                 <form onSubmit={(e) => handleSubmitFun(e)}>
@@ -45,7 +83,7 @@ const ConfirmEmail = props => {
                     </div>
                     <button type="submit">Submit</button>
                 </form>
-            </div>
+            </div> */}
         </>
     )
 }
