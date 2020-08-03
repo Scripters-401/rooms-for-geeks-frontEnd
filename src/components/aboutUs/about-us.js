0a0142778880d5dd6140ca6ccd11958d3d39d43a
EsraaMamoun <esraamamoun5@gmail.com>
2020-08-03 17:29:50 +0300

    pass(user-page) / Links(about-us)

--- src/components/aboutUs/about-us.js
+++ src/components/aboutUs/about-us.js
@@ -40,7 +40,7 @@ const AboutAs = props => {
                     actually i am passionate to learn new technologies and skills so in my free time i am trying to solve challenges and learn new things ” </q>
                                 <div className="div2">
                                     <a href="https://www.facebook.com/rowaid.sayyed" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faFacebook} size="2x" color='#416760' /></a>
-                                    <a href="https://github.com/Scripters-401" rel="noopener noreferrer" target='_blank' className="faceBook" >  <FontAwesomeIcon icon={faGithub} size="2x" color='#416760' /></a>
+                                    <a href="https://github.com/rowaidsayyed" rel="noopener noreferrer" target='_blank' className="faceBook" >  <FontAwesomeIcon icon={faGithub} size="2x" color='#416760' /></a>
                                     <a href="https://www.linkedin.com/in/rowaidsayyed/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faLinkedin} size="2x" color='#416760' /></a>
                                 </div>
                             </div>
@@ -63,9 +63,9 @@ const AboutAs = props => {
 
                                     ” My name is Esra'a Mamoun, I am a software developer with a physics background, I always had a passion for problem-solving, which is why I loved programming. When I entered an intensive programming course from Issac for 6 months, it was so full of challenges and difficulties which makes many of my colleagues left it, but for me, as far as the difficulties I faced, it was also fun. There was a project after each level of building an Integrated website within 3-4 days to sharpen what we learned during the level. We used JavaScript, CSS, HTML, JQuery, servers, databases, and an open API, we learned and accomplished a lot in a short time, I loved this stage of my life very much ” </q>
                                 <div className="div2">
-                                    <a href="https://www.facebook.com/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faFacebook} size="2x" color='#416760' /></a>
-                                    <a href="https://github.com/Scripters-401" rel="noopener noreferrer" target='_blank' className="faceBook" >  <FontAwesomeIcon icon={faGithub} size="2x" color='#416760' /></a>
-                                    <a href="https://www.linkedin.com/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faLinkedin} size="2x" color='#416760' /></a>
+                                    <a href="https://www.facebook.com/Agatha.Christie.Phoenix/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faFacebook} size="2x" color='#416760' /></a>
+                                    <a href="https://github.com/EsraaMamoun" rel="noopener noreferrer" target='_blank' className="faceBook" >  <FontAwesomeIcon icon={faGithub} size="2x" color='#416760' /></a>
+                                    <a href="https://www.linkedin.com/in/esraa-mamoun-43b4821a1/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faLinkedin} size="2x" color='#416760' /></a>
                                 </div>
                             </div>
                         </div>
@@ -88,9 +88,9 @@ const AboutAs = props => {
 
                                     ”My name is Obada Hussein i'm 24 years old .. i graduated from Yarmouk University with a major of computer science .. currently i graduated from ASAC that i took a course in software engineering and right now i'm a full stack web developer Specializes in nodeJS with a material from CodeFellows ,in this course i learned the meaning of time pressure and teamwork before learning the technical stuff . i believe that the perfect work comes from experiences more than knowledge that's why my main goal is to collect experiences as much as possible and moving from levels to higher levels ”  </q>
                                 <div className="div2">
-                                    <a href="https://www.facebook.com/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faFacebook} size="2x" color='#416760' /></a>
-                                    <a href="https://github.com/Scripters-401" rel="noopener noreferrer" target='_blank' className="faceBook" >  <FontAwesomeIcon icon={faGithub} size="2x" color='#416760' /></a>
-                                    <a href="https://www.linkedin.com/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faLinkedin} size="2x" color='#416760' /></a>
+                                    <a href="https://www.facebook.com/obada.tumaah" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faFacebook} size="2x" color='#416760' /></a>
+                                    <a href="https://github.com/Tumaho" rel="noopener noreferrer" target='_blank' className="faceBook" >  <FontAwesomeIcon icon={faGithub} size="2x" color='#416760' /></a>
+                                    <a href="https://www.linkedin.com/in/obada19/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faLinkedin} size="2x" color='#416760' /></a>
                                 </div>
                             </div>
                         </div>
@@ -119,9 +119,9 @@ const AboutAs = props => {
                                     I love challenges and I always accept it, I have been trained in two company in software and hardware,
                     I dealt with the online process for both working and learning and I have worked in teams and we create really good projects together ”   </q>
                                 <div className="div2">
-                                    <a href="https://www.facebook.com/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faFacebook} size="2x" color='#416760' /></a>
-                                    <a href="https://github.com/Scripters-401" rel="noopener noreferrer" target='_blank' className="faceBook" >  <FontAwesomeIcon icon={faGithub} size="2x" color='#416760' /></a>
-                                    <a href="https://www.linkedin.com/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faLinkedin} size="2x" color='#416760' /></a>
+                                    <a href="https://www.facebook.com/balqees.qasem.9" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faFacebook} size="2x" color='#416760' /></a>
+                                    <a href="https://github.com/Balqeesqasem" rel="noopener noreferrer" target='_blank' className="faceBook" >  <FontAwesomeIcon icon={faGithub} size="2x" color='#416760' /></a>
+                                    <a href="https://www.linkedin.com/in/balqees-qasem-4a8782173/" className="faceBook" rel="noopener noreferrer" target='_blank'>  <FontAwesomeIcon icon={faLinkedin} size="2x" color='#416760' /></a>
                                 </div>
                             </div>
                         </div>
