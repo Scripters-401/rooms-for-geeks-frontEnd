26373d906ecd197f9f75a823b1e89422cb61f223
EsraaMamoun <esraamamoun5@gmail.com>
2020-08-04 21:13:50 +0300

    still a lot of work

--- src/components/room/room.js
+++ src/components/room/room.js
@@ -88,13 +88,13 @@ const Room = props => {
 
     }
 
-    const someMeothod = ()=>{
+    const someMeothod = () => {
         let z = document.body.scrollHeight;
-        let  x = window.scrollY
-        let height = document.body.scrollHeight-window.scrollY
-        if(height<830){
+        let x = window.scrollY
+        let height = document.body.scrollHeight - window.scrollY
+        if (height < 830) {
             props.updateChatScroll(true)
-        }else{
+        } else {
             props.updateChatScroll(false)
 
         }
@@ -102,75 +102,86 @@ const Room = props => {
 
     return (
         <>
-       {/* {onScroll=e =>someMeothod()} */}
-            <div className="bodyDiv" onWheel={e =>someMeothod()} >
+            {/* {onScroll=e =>someMeothod()} */}
+            <div className="bodyDiv" onWheel={e => someMeothod()} >
                 <div className='roomData'>
-                    {props.room.redirectAfterDelete ? (<Redirect to="/user-Home" />) : null}
-                    <div id="room-data">
-                        <div className="quwsHead">
-                            <Show condition={props.room.roomAdmin}>
-                                <Auth capability="master-room">
-                                    <button className='deleteRoom' onClick={e => deleteRoom()}> Delete Room</button>
-                                </Auth>
-                            </Show>
-                            <h2 id='roon-name'>{props.room.roomData.RData && props.room.roomData.RData.roomName ? props.room.roomData.RData.roomName : null}</h2>
-                            <Show condition={props.room.favOrNot}>
-                                <span className={`addToFav-${props.room.favOrNot}`} onClick={e => removefromFav()}>
+                    <div className="containerRoom">
+                        {props.room.redirectAfterDelete ? (<Redirect to="/user-Home" />) : null}
+                        <div id="room-data">
+                            <div className="quwsHead">
 
-                                    <div class="slideThree">
-                                        <input type="checkbox" value="None" id="slideThree" name="check" checked />
+                                <Show condition={props.room.favOrNot}>
+                                    <span className={`addToFav-${props.room.favOrNot}`} onClick={e => removefromFav()}>
+
+                                        <div class="slideThree">
+                                            <input type="checkbox" value="None" id="slideThree" name="check" checked />
+                                            <label for="slideThree"></label>
+                                        </div>
+
+                                    </span>
+                                </Show>
+                                <Show condition={!props.room.favOrNot}>
+                                    <span className={`addToFav-${props.room.favOrNot}`} onClick={e => addToFav()}>  <div class="slideThree">
+                                        <input type="checkbox" value="None" id="slideThree" name="check" />
                                         <label for="slideThree"></label>
-                                    </div>
-
-                                </span>
-                            </Show>
-                            <Show condition={!props.room.favOrNot}>
-                                <span className={`addToFav-${props.room.favOrNot}`} onClick={e => addToFav()}>  <div class="slideThree">
-                                    <input type="checkbox" value="None" id="slideThree" name="check" />
-                                    <label for="slideThree"></label>
-                                </div></span>
-                            </Show>
+                                    </div></span>
+                                </Show>
+                                <h2 id='roon-name'>{props.room.roomData.RData && props.room.roomData.RData.roomName ? props.room.roomData.RData.roomName : null} room</h2>
+                                <Show condition={props.room.roomAdmin}>
+                                    <Auth capability="master-room">
+                                        <button className='deleteRoom' onClick={e => deleteRoom()}> Delete Room</button>
+                                    </Auth>
+                                </Show>
+                            </div>
                         </div>
-
-                    </div>
-
-                    <div className='courseData'>
-                        <h2 className="courseName"> {props.room.roomData && props.room.roomData.courseData ? props.room.roomData.courseData.courseName : null}</h2>
-                        <p className="Discription"> {props.room.roomData && props.room.roomData.courseData ? props.room.roomData.courseData.discription : null}</p>
-
-                    </div>
-
-
-
-                    <QA />
-                    <div className="buttonRoom">
-                        <Show condition={!(props.room.roomData.renderedQuiz && props.room.roomData.renderedQuiz.constructor === Object && Object.keys(props.room.roomData.renderedQuiz).length === 0)}>
-                            <Link className="aHrefLinkdds" to="/take-quiz"> <button className="TakeQuizAndToto" >Take Quiz</button></Link>
-                        </Show>
-
-                        <Show condition={(props.room.roomData.courseData && props.room.roomData.courseData.tutorial)}>
-                            <a className="aHrefLinkdds" href={`https://${props.room.roomData.courseData && props.room.roomData.courseData.tutorial ? props.room.roomData.courseData.tutorial : null}`} rel="noopener noreferrer" target='_blank'><button className="TakeQuizAndToto" >Tutorial</button></a>
-                        </Show>
-                    </div>
-                </div>
-                <div className="chatSet" >
-                    <Chat />
-                </div>
-                <div className="roomDataFor">
-                    <p className=" CreatedBy">
-                        Created By :
-                        {props.room.adminName}
-                    </p>
-                    <p className=" CreatedByOn">
-                        On :
+                        {/* <div className="roomDataFor">
+                            <p className="Topic">Topic: {props.room.roomData && props.room.roomData.courseData ? props.room.roomData.courseData.topic : null}</p>
+                            <p className=" CreatedBy">
+                                Created By: {props.room.adminName} at {props.room.roomData && props.room.roomData.RData && props.room.roomData.RData.createdTime ? props.room.roomData.RData.createdTime.slice(0, 10) : null}
+                            </p> */}
+                            {/* <p className=" CreatedByOn">
+                                On :
                         {props.room.roomData && props.room.roomData.RData && props.room.roomData.RData.createdTime ? props.room.roomData.RData.createdTime.slice(0, 10) : null}
-                    </p>
-                    <p className="Topic">Topic: {props.room.roomData && props.room.roomData.courseData ? props.room.roomData.courseData.topic : null}</p>
-                    {/* <p>
+                            </p> */}
+
+                            {/* <p>
                         Public: {props.room.roomData && props.room.roomData.RData ? `${props.room.roomData.RData.public || props.room.roomData.RData.publicc}` : null}
                     </p> */}
 
+                        {/* </div> */}
+
+                        <div className='courseData'>
+                            <h2 className="courseName">Course {props.room.roomData && props.room.roomData.courseData ? props.room.roomData.courseData.courseName : null}</h2>
+                            <p className="Discription">Discription {props.room.roomData && props.room.roomData.courseData ? props.room.roomData.courseData.discription : null}</p>
+
+                        </div>
+                    </div>
+
+
+
+                    <div className="QA-Component">
+                        <aside className="quiaAndTutorial">
+                            
+                            <div className="buttonRoom">
+                                <Show condition={!(props.room.roomData.renderedQuiz && props.room.roomData.renderedQuiz.constructor === Object && Object.keys(props.room.roomData.renderedQuiz).length === 0)}>
+                                    <Link className="aHrefLinkdds" to="/take-quiz"> <button className="TakeQuizAndToto" >Take Quiz</button></Link>
+                                </Show>
+
+                                <Show condition={(props.room.roomData.courseData && props.room.roomData.courseData.tutorial)}>
+                                    <a className="aHrefLinkdds" href={`https://${props.room.roomData.courseData && props.room.roomData.courseData.tutorial ? props.room.roomData.courseData.tutorial : null}`} rel="noopener noreferrer" target='_blank'><button className="TakeQuizAndToto" >Tutorial</button></a>
+                                </Show>
+                            </div>
+                        </aside>
+                        <div className="just-QA"><QA /></div>
+                    </div>
+
+
+
+                    <div className="chatSet" >
+                        <Chat />
+                    </div>
                 </div>
+
 
             </div>
 
