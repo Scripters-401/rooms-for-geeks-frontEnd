2d6424071071f9eae91c77feb49658b5b4a5ae11
Balqeesqasem <balqeesmohammadq.95@gmail.com>
2020-08-06 04:13:46 +0300

    addReviwForm

--- src/components/interviewReview/interviewReview.js
+++ src/components/interviewReview/interviewReview.js
@@ -297,7 +297,7 @@ const AllInterviewR = props => {
                 <div id="btnInt">
                     <button id="btnInterview" className="custom-btn btn-16" onClick={(e) => props.divBlockNone('divBlock')} >Add Interview Review</button>
                 </div>
-                <div id={props.allInterview.blockNone} className="modall">s
+                <div id={props.allInterview.blockNone} className="modall">
                     <span onClick={(e) => props.divBlockNone('divNone')} className="close" title="Close Modal">&times;</span>
 
 
